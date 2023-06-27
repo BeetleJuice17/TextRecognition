70fe90c2acee09ca3c034f676a1da07babe6ee90
Beetlejuice17 <sbloodrain@yandex.ru>
2023-06-27 16:58:03 +0300

    добавил сохранение загруженной фотографии во временное харнилище

--- app/src/main/java/com/example/textrecognition/activity/OcrActivity.java
+++ app/src/main/java/com/example/textrecognition/activity/OcrActivity.java
@@ -2,15 +2,74 @@ package com.example.textrecognition.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ImageView;
 
 import com.example.textrecognition.R;
 
-public class OcrActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
+public class OcrActivity extends AppCompatActivity {
+    private Bitmap yourBitmap;
+    private String imagePath;
+
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr);
+
+        imageView = findViewById(R.id.imageView);
     }
+
+    private String saveImage(Bitmap image) {
+        String savedImagePath = null;
+
+        String imageFileName = "JPEG_" + System.currentTimeMillis() + ".jpg";
+        File storageDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/YourAppName");
+        boolean success = true;
+        if (!storageDir.exists()) {
+            success = storageDir.mkdirs();
+        }
+
+        if (success) {
+            File imageFile = new File(storageDir, imageFileName);
+            savedImagePath = imageFile.getAbsolutePath();
+            try {
+                OutputStream fOut = new FileOutputStream(imageFile);
+                image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                fOut.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return savedImagePath;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        String imagePath = saveImage(yourBitmap);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        File file = new File(imagePath);
+        file.delete();
+    }
+
 }
