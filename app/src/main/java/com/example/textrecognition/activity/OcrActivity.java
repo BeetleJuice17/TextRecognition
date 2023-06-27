11a60ac6bc7632b6c53939b769603c1f63c0480f
Beetlejuice17 <sbloodrain@yandex.ru>
2023-06-27 16:34:21 +0300

    добавил передачу файлов через соц сети

--- app/src/main/java/com/example/textrecognition/activity/OcrActivity.java
+++ app/src/main/java/com/example/textrecognition/activity/OcrActivity.java
@@ -1,10 +1,15 @@
 package com.example.textrecognition.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.textrecognition.R;
+
+import java.io.File;
 
 public class OcrActivity extends AppCompatActivity {
 
@@ -13,4 +18,38 @@ public class OcrActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr);
     }
+
+    private void shareFileViaVK() {
+        String filePath = "path_to_your_file";
+        File file = new File(filePath);
+        Uri fileUri = FileProvider.getUriForFile(this, "com.your.package.name.fileprovider", file);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("file/*");
+        intent.putExtra(Intent.EXTRA_STREAM, fileUri);
+        intent.setPackage("com.vkontakte.android");
+        startActivity(Intent.createChooser(intent, "Share via"));
+    }
+
+    private void shareFileViaWhatsApp() {
+        String filePath = "path_to_your_file";
+        File file = new File(filePath);
+        Uri fileUri = FileProvider.getUriForFile(this, "com.your.package.name.fileprovider", file);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("file/*");
+        intent.putExtra(Intent.EXTRA_STREAM, fileUri);
+        intent.setPackage("com.whatsapp");
+        startActivity(Intent.createChooser(intent, "Share via"));
+    }
+
+    private void shareFileViaTelegram() {
+        String filePath = "path_to_your_file";
+        File file = new File(filePath);
+        Uri fileUri = FileProvider.getUriForFile(this, "com.your.package.name.fileprovider", file);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("file/*");
+        intent.putExtra(Intent.EXTRA_STREAM, fileUri);
+        intent.setPackage("org.telegram.messenger");
+        startActivity(Intent.createChooser(intent, "Share via"));
+    }
+
 }
