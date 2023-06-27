b52b929a6ea615f51f96bd67b90823c8da1df066
scherlok6 <skibakol@yandex.ru>
2023-06-27 16:25:05 +0300

    добавлена функция по выгрузке файла с результатом

--- app/src/main/java/com/example/textrecognition/activity/OcrActivity.java
+++ app/src/main/java/com/example/textrecognition/activity/OcrActivity.java
@@ -6,11 +6,28 @@ import android.os.Bundle;
 
 import com.example.textrecognition.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class OcrActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr);
+    }
+
+    public File save_file(String result){
+        try {
+            String filename = "result.txt";
+            File file = new File(getFilesDir(), filename);
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(result.getBytes());
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
